164a9ff70ddc0d69ef2a911103f0c04fb04ef9df
Leandro <leoviolinist22@gmail.com>
2022-11-17 15:24:59 -0300

    adicionando codigo dos nos

--- main.cpp
+++ main.cpp
@@ -6,6 +6,61 @@ using namespace std;
 #include "hungarian.h"
 #include <vector>
 
+
+
+/* Indica a estrutura do nó	*/
+typedef struct {
+
+	vector < pair<int, int >> forbidden_arcs; // Lista de arcos proibidos no nó
+	vector < vector < int > > subtours; // Conjunto de subtours da solução
+	double lower_bound; // Custo total da solução do algoritmo hungaro
+	int chosen; // Indice do menor subtour
+	double feasible; // Indica se a solução AP_TSP é viável
+
+}NodeInfo;
+
+
+
+void instanceNode(NodeInfo* node, vector < vector < int > >& subtours, double obj_value) {
+	
+	/* Inicializa os subtours e o valor do lower bound dados	*/
+	node->subtours = subtours;
+	node->lower_bound = obj_value;
+	
+	cout << "here0" << endl;
+	int lower_arc = subtours[0].size();
+	/* Verifica o tamanho do menor arco	*/
+	for(auto line : subtours) {
+
+		if(line.size() < lower_arc) {
+
+			lower_arc = line.size();
+		}
+	}
+	
+	cout << "here1" << endl;
+	/* Restringe todos os vértices que estão no menor arco, e vamos restringir todos os nós daquele subtour*/
+	int j = 0;
+	for(vector < int > subtour : subtours) {
+		
+		if(subtour.size() == lower_arc) {
+			node->chosen = j; // O primeiro subtour com o tamanho do menor arco, será escolhido	
+			for(int i = 0; i < subtours.size() - 1; i++) {
+				node->forbidden_arcs.push_back(make_pair(subtour[i], subtour[i + 1]));
+			} 
+
+		}
+		
+		j++;
+	}
+	
+	cout << "here";
+	for(int i = 0; i  < node->forbidden_arcs.size(); i++) {
+
+			cout << node->forbidden_arcs[i].first << " " << node->forbidden_arcs[i].second << endl;
+	}
+}
+
 int main(int argc, char** argv) {
 
 	Data * data = new Data(argc, argv[1]);
@@ -95,18 +150,24 @@ int main(int argc, char** argv) {
 	}
 	
 	sort(subtours.begin(), subtours.end(), [=](auto  A, auto B){
-			return A.size() >  B.size();
+			return A.size() >  B.size(); 
 	}
-	);
+	);   
+	
 
-	for(auto line : subtours) {
-		
+
+	
+	for(auto line: subtours) {
+
 		for(int i = 0; i < line.size(); i++) {
 
 			cout << line[i] << " ";
 		}
 		cout << endl;
-			
 	}
+	
+	NodeInfo* node = new NodeInfo();
+	cout << "here-1" << endl;
+	instanceNode(node, subtours, obj_value);
 	return 0;
 }
